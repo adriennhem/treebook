d7572b92a767a239d02faf6847c8b3b048d2755a
Adrien Nhem <adrien.nhem@gmail.com>
2014-08-30 13:38:27 +0100

    updated gemfile for heroku

--- Gemfile
+++ Gemfile
@@ -16,18 +16,13 @@ gem 'simple_form'
 
 
 gem 'sdoc', '~> 0.4.0',          group: :doc
-
 gem 'spring',        group: :development
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+group :development, :test do
+     gem 'sqlite3'
+end
 
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
-# Use debugger
-# gem 'debugger', group: [:development, :test]
-
+group :production do
+     gem 'pg'
+     gem 'rails_12factor'
+end
--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,5 +1,6 @@
 class StatusesController < ApplicationController
   before_action :set_status, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /statuses
   # GET /statuses.json
@@ -14,7 +15,7 @@ class StatusesController < ApplicationController
 
   # GET /statuses/new
   def new
-    @status = Status.new
+    @status = current_user.statuses.build
   end
 
   # GET /statuses/1/edit
@@ -24,7 +25,7 @@ class StatusesController < ApplicationController
   # POST /statuses
   # POST /statuses.json
   def create
-    @status = Status.new(status_params)
+    @status = current_user.statuses.build(status_params)
 
     respond_to do |format|
       if @status.save
